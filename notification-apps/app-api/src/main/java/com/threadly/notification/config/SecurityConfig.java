f9d1425d15247c8ed116ae53f0a6a311ebc223c1
KimGyuBek <rlarbqor00@naver.com>
2025-09-14 02:01:05 +0900

    chore:TLY-107 배포 테스트

--- notification-apps/app-api/src/main/java/com/threadly/notification/config/SecurityConfig.java
+++ notification-apps/app-api/src/main/java/com/threadly/notification/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig {
     http.authorizeHttpRequests(
         auth -> auth
             .requestMatchers(
+                "/actuator/**",
                 "/api/test/kafka",
                 "/ws/**")
             .permitAll()
